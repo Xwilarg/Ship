de817d044f0d60564abcc0d4b8782e291720b5fa
Xwilarg <xwilarg@yahoo.fr>
2020-09-21 22:23:12 +0200

    Add KanColle json

--- js/nodes.js
+++ js/nodes.js
@@ -1,14 +1,10 @@
 var nodes = new vis.DataSet([
-    { id: 1, label: "Node 1" },
-    { id: 2, label: "Node 2" }
+    { id: 1, label: "Node 1", color: "lightblue" },
+    { id: 2, label: "Node 2", color: "lightblue" }
 ]);
 
 var edges = new vis.DataSet([
-    { from: 1, to: 3 },
-    { from: 1, to: 2 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 },
-    { from: 3, to: 3 }
+    { from: 1, to: 2 }
 ]);
 
 var container = document.getElementById("nodes");
