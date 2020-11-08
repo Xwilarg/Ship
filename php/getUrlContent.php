fc05469ed0732b7c3bc56206cc37663b93b8f136
Xwilarg <xwilarg@yahoo.fr>
2020-11-08 16:09:34 +0100

    Fix security issue

--- php/getUrlContent.php
+++ php/getUrlContent.php
@@ -1,9 +1,16 @@
 <?php
 
-$options = [
-    "http" => [
-        "method" => "GET"
-    ]
-];
-$context = stream_context_create($options);
-echo(file_get_contents($_GET['url'], false, $context));
+if (substr($_GET['url'], 0, 4) !== "http")
+{
+    echo("Invalid operation");
+}
+else
+{
+    $options = [
+        "http" => [
+            "method" => "GET"
+        ]
+    ];
+    $context = stream_context_create($options);
+    echo(file_get_contents($_GET['url'], false, $context));
+}
