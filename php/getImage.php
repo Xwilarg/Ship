cecf11be7c08aa6167c8ef7e1d7888cc823436ff
Xwilarg <xwilarg@yahoo.fr>
2020-11-20 05:11:59 +0100

    Fix invalid extension for Twitter links

--- php/getImage.php
+++ php/getImage.php
@@ -15,7 +15,7 @@ else
     $characterName2 = $_GET['characterName2'];
     $imageLink = $_GET['imageLink'];
     $names = $characterName < $characterName2 ? ($characterName . "_" . $characterName2)  : ($characterName2 . "_" . $characterName);
-    $format = pathinfo(
+    $format = substr($_GET['imageLink'], -4) === "=jpg" ? "jpg" : pathinfo(
         parse_url($_GET['imageLink'], PHP_URL_PATH), 
         PATHINFO_EXTENSION
     );
