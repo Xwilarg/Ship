ca4daf44feae4920ffd003e3e4ce531b77dccff6
Xwilarg <xwilarg@yahoo.fr>
2021-01-09 01:07:48 +0100

    Fix security issue

--- php/getInfoJson.php
+++ php/getInfoJson.php
@@ -1,5 +1,12 @@
 <?php
-$path = __DIR__ . "/../Ship_data/" . $_GET['folder'] . "/info.json";
-if (file_exists($path)) {
-    echo file_get_contents($path);
+if (strpos($_GET['folder'], "..") !== false)
+{
+    http_response_code(400);
+}
+else
+{
+    $path = __DIR__ . "/../Ship_data/" . $_GET['folder'] . "/info.json";
+    if (file_exists($path)) {
+        echo file_get_contents($path);
+    }
 }
