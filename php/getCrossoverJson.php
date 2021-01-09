9d5a15344ab967c6ad051d64323c390c5aa76f91
Xwilarg <xwilarg@yahoo.fr>
2021-01-09 02:57:18 +0100

    Fix security issue

--- php/getCrossoverJson.php
+++ php/getCrossoverJson.php
@@ -1,2 +1,9 @@
 <?php
-echo file_get_contents(__DIR__ . "/../Ship_data/" . $_GET['folder'] . "/crossover.json");
+if (strpos($_GET['folder'], "..") !== false)
+{
+    http_response_code(400);
+}
+else
+{
+    echo file_get_contents(__DIR__ . "/../Ship_data/" . $_GET['folder'] . "/crossover.json");
+}
